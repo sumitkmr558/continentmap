6a1b9166cb39f418a32e99f72e37ea3ac1797119
Sumit kumar <sumitkumar@Sumits-MacBook-Pro.local>
2025-04-28 14:05:33 +0530

    Your update message

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <h1>World Map</h1>
-      <WorldMap />
-      {/* <GlobeComponent /> */}
+      {/* <WorldMap /> */}
+      <GlobeComponent />
     </div>
   );
 }
